3df9dfd35edee6c4a28efbaf934325dbdeebe532
zkhugger <yanuarputra036@gmail.com>
2026-05-31 20:48:25 +0700

    style: format withdrawer.sol

--- web3-token/contracts/Withdrawer.sol
+++ web3-token/contracts/Withdrawer.sol
@@ -9,14 +9,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Withdrawer is EIP712, ReentrancyGuard, Ownable {
     bytes32 private constant WITHDRAWAL_TYPEHASH =
-        keccak256("Withdrawal(address wallet,uint256 amount,uint256 nonce,uint256 deadline)");
+        keccak256(
+            "Withdrawal(address wallet,uint256 amount,uint256 nonce,uint256 deadline)"
+        );
 
     IERC20 public token;
     address public authorizedSigner;
 
     mapping(address => uint256) public nonces;
 
-    event WithdrawalClaimed(address indexed wallet, uint256 amount, uint256 nonce);
+    event WithdrawalClaimed(
+        address indexed wallet,
+        uint256 amount,
+        uint256 nonce
+    );
     event SignerUpdated(address indexed oldSigner, address indexed newSigner);
 
     error DeadlineExpired();
@@ -42,12 +48,15 @@ contract Withdrawer is EIP712, ReentrancyGuard, Ownable {
         if (block.timestamp > deadline) revert DeadlineExpired();
         if (nonces[msg.sender] != nonce) revert InvalidNonce();
 
-        bytes32 structHash = keccak256(abi.encode(WITHDRAWAL_TYPEHASH, msg.sender, amount, nonce, deadline));
+        bytes32 structHash = keccak256(
+            abi.encode(WITHDRAWAL_TYPEHASH, msg.sender, amount, nonce, deadline)
+        );
         bytes32 digest = _hashTypedDataV4(structHash);
         address signer = ECDSA.recover(digest, signature);
         if (signer != authorizedSigner) revert InvalidSignature();
 
-        if (token.balanceOf(address(this)) < amount) revert InsufficientPoolBalance();
+        if (token.balanceOf(address(this)) < amount)
+            revert InsufficientPoolBalance();
 
         nonces[msg.sender] = nonce + 1;
 
@@ -57,7 +66,10 @@ contract Withdrawer is EIP712, ReentrancyGuard, Ownable {
     }
 
     function depositTokens(uint256 amount) external onlyOwner {
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "Transfer failed"
+        );
     }
 
     function poolBalance() external view returns (uint256) {
