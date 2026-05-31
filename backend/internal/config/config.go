a33b6483eab19f42134d3ca282f34f531be5fee0
zkhugger <yanuarputra036@gmail.com>
2026-05-31 20:48:25 +0700

    chore: add env configs for platform fee, google oauth, resend, rate limits

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -26,6 +26,10 @@ type Config struct {
 	JWTRefreshSecret   string  `mapstructure:"JWT_REFRESH_SECRET"`
 	RateLimitRedirect  int     `mapstructure:"RATE_LIMIT_REDIRECT"`
 	RateLimitCreate    int     `mapstructure:"RATE_LIMIT_CREATE"`
+	RateLimitDefault   int     `mapstructure:"RATE_LIMIT_DEFAULT"`
+	RateLimitAuth      int     `mapstructure:"RATE_LIMIT_AUTH"`
+	RateLimitMedia     int     `mapstructure:"RATE_LIMIT_MEDIA"`
+	RateLimitWithdrawal int     `mapstructure:"RATE_LIMIT_WITHDRAWAL"`
 	GeoIPDBPath        string  `mapstructure:"GEOIP_DB_PATH"`
 	AllowedOrigins     string  `mapstructure:"ALLOWED_ORIGINS"`
 	NodeRPCURL         string  `mapstructure:"NODE_RPC_URL"`
@@ -50,6 +54,13 @@ type Config struct {
 	BridgeHMACSecret   string  `mapstructure:"BRIDGE_HMAC_SECRET"`
 	QualityMinScore    float64 `mapstructure:"QUALITY_MIN_SCORE"`
 	MinSessionMs       int64   `mapstructure:"MIN_SESSION_MS"`
+	PlatformFee        float64 `mapstructure:"PLATFORM_FEE"`
+	GoogleClientID     string  `mapstructure:"GOOGLE_CLIENT_ID"`
+	GoogleClientSecret string  `mapstructure:"GOOGLE_CLIENT_SECRET"`
+	GoogleRedirectURL  string  `mapstructure:"GOOGLE_REDIRECT_URL"`
+	ResendAPIKey       string  `mapstructure:"RESEND_API_KEY"`
+	ResendFrom         string  `mapstructure:"RESEND_FROM"`
+	FrontendURL        string  `mapstructure:"FRONTEND_URL"`
 }
 
 func (c *Config) IsDev() bool {
@@ -62,6 +73,11 @@ func Load() (*Config, error) {
 	viper.AutomaticEnv()
 
 	viper.SetDefault("ENV", constants.EnvDevelopment)
+	viper.SetDefault("PLATFORM_FEE", 0.005)
+	viper.SetDefault("RATE_LIMIT_DEFAULT", 1200)
+	viper.SetDefault("RATE_LIMIT_AUTH", 60)
+	viper.SetDefault("RATE_LIMIT_MEDIA", 20)
+	viper.SetDefault("RATE_LIMIT_WITHDRAWAL", 10)
 
 	bindEnvs(Config{})
 
@@ -89,7 +105,7 @@ func Load() (*Config, error) {
 		cfg.ContractToken = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 		cfg.ContractPayment = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 		cfg.ContractFaucet = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
-		cfg.ContractWithdrawer = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+		cfg.ContractWithdrawer = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
 		cfg.S3Endpoint = "http://garage:3900"
 		cfg.S3Bucket = "short-url-ads"
 		cfg.S3PublicURL = "http://short-url-ads.web.localhost:3902"
